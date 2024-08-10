8ec38c1a627edc87407503189c8d8b0729b0b300
Letícia Desiderio <leticia.desiderio@bnpsolucoes.com.br>
2024-08-10 20:50:33 -0300

    refactor(user): create

--- alimentar-bem/src/components/CreateUser.js
+++ alimentar-bem/src/components/CreateUser.js
@@ -2,46 +2,46 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 const CreateUser = () => {
-  const [userData, setUserData] = useState({
-    name: '',
-    email: '',
-    password: ''
-  });
-
-  const handleChange = (e) => {
-    setUserData({
-      ...userData,
-      [e.target.name]: e.target.value
+    const [userData, setUserData] = useState({
+        name: '',
+        email: '',
+        password: ''
     });
-  };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post('http://localhost:5178/user', userData);
-      console.log('User created:', response.data);
-    } catch (error) {
-      console.error('There was an error creating the user!', error);
-    }
-  };
+    const handleChange = (e) => {
+        setUserData({
+            ...userData,
+            [e.target.name]: e.target.value
+        });
+    };
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Name</label>
-        <input type="text" name="name" value={userData.name} onChange={handleChange} />
-      </div>
-      <div>
-        <label>Email</label>
-        <input type="email" name="email" value={userData.email} onChange={handleChange} />
-      </div>
-      <div>
-        <label>Password</label>
-        <input type="password" name="password" value={userData.password} onChange={handleChange} />
-      </div>
-      <button type="submit">Create User</button>
-    </form>
-  );
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post(`${process.env.REACT_APP_API_BASE_URL}/user`, userData);
+            console.log('User created:', response.data);
+        } catch (error) {
+            console.error('There was an error creating the user!', error);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>Nome</label>
+                <input type="text" name="name" value={userData.name} onChange={handleChange} />
+            </div>
+            <div>
+                <label>Email</label>
+                <input type="email" name="email" value={userData.email} onChange={handleChange} />
+            </div>
+            <div>
+                <label>Senha</label>
+                <input type="password" name="password" value={userData.password} onChange={handleChange} />
+            </div>
+            <button type="submit">Enviar</button>
+        </form>
+    );
 };
 
 export default CreateUser;
